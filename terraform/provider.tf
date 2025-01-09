d9af1fd4d9c2eafa368bae5f48ba4f986f3bd41a
vicky <vbvignesh03@gmail.com>
2025-01-09 15:22:22 +0530

    resolved conflicts

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,3 @@
 provider "aws" {
   region = "us-east-1"  # You can change this based on your region
 }
-
-resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "wordpress-db-credentials"
-}
-
-resource "aws_secretsmanager_secret_version" "db_credentials_version" {
-  secret_id     = aws_secretsmanager_secret.db_credentials.id
-  secret_string = jsonencode({
-    username = "wordpress_user",
-    password = "@admin1"
-  })
-}
